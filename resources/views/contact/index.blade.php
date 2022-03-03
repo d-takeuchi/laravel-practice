6053a562dc246b460dbcb1e5db47189a7fa3ef5e
d-takeuchi <kuronk.kuro3@gmail.com>
2022-03-03 18:51:20 +0900

    store　保存処理の記述

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -3,5 +3,11 @@
 
 @section('content')
 <a href="{{route('contact.create')}}">新規登録</a>
+
+@foreach($contacts as $contact)
+{{$contact->id}}
+{{$contact->your_name}}
+
+@endforeach
 indexです
 @endsection
